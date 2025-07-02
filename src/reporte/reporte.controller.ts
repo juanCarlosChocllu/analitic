bac783d5b03b11dfcf40e9dd2232a5206459f9e8
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-07-02 15:18:51 -0400

    cambios

--- src/reporte/reporte.controller.ts
+++ src/reporte/reporte.controller.ts
@@ -8,14 +8,17 @@ export class ReporteController {
   constructor(private readonly reporteService: ReporteService) {}
 
   @Post()
-  @Public()
   async realizarDescarga(@Body()fechaDto:DescargarDto) {     
     return this.reporteService.realizarDescarga(fechaDto);
   }
-
   @Post('receta')
-  @Public()
   async descargarReceta(@Body()fechaDto:DescargarDto) {     
     return this.reporteService.descargarReceta(fechaDto);
+  }
+
+  @Post('actualizar')
+  @Public()
+  async actualizarVentas(@Body()fechaDto:DescargarDto){
+    return this.reporteService.actualizarVentas(fechaDto)
   }
 }
